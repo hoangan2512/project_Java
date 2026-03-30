480a9b99232ac612c0dc61c6763fde4b2ab79c02
hoangan2512 <hoangan1m8@gmail.com>
2026-03-30 10:37:38 +0700

    test database

--- auction-server/src/main/java/server/Main.java
+++ auction-server/src/main/java/server/Main.java
@@ -1,7 +1,10 @@
 package server;
 
 import server.repository.DatabaseConnection;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
