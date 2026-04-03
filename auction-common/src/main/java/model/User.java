9b4f9ae66afc9381f1d09119e5306f83d4aea3b1
SolomoHal <ontaphochanhnguyenquanganh@gmail.com>
2026-04-03 15:41:47 +0700

    update constructor bidder, seller, admin

--- auction-common/src/main/java/model/User.java
+++ auction-common/src/main/java/model/User.java
@@ -58,6 +58,7 @@ class Seller extends User {
     }
 }
 
+
 // Lớp Admin: Quản lý hệ thống
 class Admin extends User {
 
@@ -72,7 +73,7 @@ class Admin extends User {
         System.out.println("[Admin] ID: " + id + " | Tên: " + getName() + " | Quyền: " + getRole());
     }
 
-    
+
     // Chức năng riêng của Admin (Ví dụ: Khóa người dùng)
     public void blockUser(User user) {
         System.out.println("Admin " + getName() + " đã khóa người dùng: " + user.getName());
