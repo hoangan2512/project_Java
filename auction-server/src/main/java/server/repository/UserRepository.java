657d06834ff89dd404f53d7af038c12f2989568d
hoangan2512 <hoangan1m8@gmail.com>
2026-04-07 10:47:48 +0700

    update database

---
+++ auction-server/src/main/java/server/repository/UserRepository.java
@@ -0,0 +1,63 @@
+package server.repository;
+
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserRepository {
+    public boolean addUser(User user) {
+        String sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getInstance();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getRole());
+
+            int rowsAffected = pstmt.executeUpdate();
+
+            return rowsAffected > 0;
+
+        } catch (Exception e) {
+            System.out.println("Lỗi khi thêm User vào Database!");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public User login(String username, String password) {
+
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+        try (Connection conn = DatabaseConnection.getInstance();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                String role = rs.getString("role");
+                User loggedInUser = User.createUser(
+                        role,
+                        rs.getInt("id"),
+                        rs.getString("username"),
+                        rs.getString("password")
+                    );
+
+                System.out.println("Đăng nhập thành công: " + username);
+                return loggedInUser;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Lỗi truy vấn khi đăng nhập!");
+            e.printStackTrace();
+        }
+
+        System.out.println("Sai tên đăng nhập hoặc mật khẩu: " + username);
+        return null;
+    }
+}
