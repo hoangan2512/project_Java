30a6a3a89f26c780d7cc8d8c5546c412b3032977
SolomoHal <ontaphochanhnguyenquanganh@gmail.com>
2026-05-17 01:34:54 +0700

    Ok chưa con vợ dương

--- auction-server/src/main/java/server/Main.java
+++ auction-server/src/main/java/server/Main.java
@@ -41,7 +41,7 @@ public class Main {
                 "item_id INTEGER NOT NULL, " +
                 "start_time DATETIME NOT NULL, " +
                 "end_time DATETIME NOT NULL, " +
-                "status TEXT DEFAULT 'WAITING', " + // WAITING, RUNNING, FINISHED
+                "status TEXT DEFAULT 'PENDING_APPROVAL', " + // PENDING_APPROVAL, WAITING, RUNNING, FINISHED
                 "current_price DOUBLE DEFAULT 0.0, " +
                 "highest_bidder_id INTEGER, " +
                 "FOREIGN KEY(item_id) REFERENCES items(id), " +
