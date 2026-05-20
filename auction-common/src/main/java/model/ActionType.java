e2ab4794f556e71667b1e2b25dba37f97de1b0a6
SolomoHal <ontaphochanhnguyenquanganh@gmail.com>
2026-05-21 00:58:21 +0700

    Chạy thành công autobid và antisniping algorithm

--- auction-common/src/main/java/model/ActionType.java
+++ auction-common/src/main/java/model/ActionType.java
@@ -3,28 +3,29 @@ package model;
 public enum ActionType {
     LOGIN_BIDDER,
     LOGIN_SELLER,
-    LOGIN_ADMIN,    
+    LOGIN_ADMIN,
     LOGOUT,
     REGISTER,
     BID,
-    REGISTER_AUTO_BID, // Hành động mới cho auto bid
+    REGISTER_AUTOBID,
+    UNREGISTER_AUTOBID,
     GET_BID_HISTORY,
     GET_LIST,
     GET_ITEM_DETAIL,
     NOTIFY_NEW_PRICE,
     AUCTION_END,
-    AUCTION_EXTENDED,  // Bổ sung: Thông báo gia hạn phiên đấu giá (Anti-Sniping)
+    AUCTION_EXTENDED,
     CREATE_ITEM,
     CHECK_DUPLICATE_NAME,
     CUSTOM_SEARCH,
     GET_SELLER_NAME,
     GOOGLE_LOGIN,
-    
-    // --- Các hành động dành riêng cho Admin ---
+
     ADMIN_GET_ALL_USERS,
     ADMIN_BAN_USER,
     ADMIN_UNBAN_USER,
     ADMIN_APPROVE_ITEM,
     ADMIN_REJECT_ITEM,
-    ADMIN_GET_PENDING_ITEMS, ADMIN_STOP_AUCTION // Thêm hành động mới để dừng phiên đấu giá
+    ADMIN_GET_PENDING_ITEMS,
+    ADMIN_STOP_AUCTION
 }
